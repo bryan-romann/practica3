334c03a9b6723f74013f679212c30d39903a9d8e
Bryan Román Solano <bryanfernando.romansolano@gmail.com>
2025-06-20 21:07:03 -0600

    Practica 03 Grupo 06

---
+++ src/main/java/com/practica3/practica3/ProjectConfig.java
@@ -0,0 +1,33 @@
+
+package com.practica3.practica3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registro){
+            registro.addViewController("/").setViewName("index");
+            registro.addViewController("/arboles").setViewName("arboles");      
+    }
+    
+    /* El siguiente método se utilizar para publicar en la nube, independientemente  */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    }
+    
+}
